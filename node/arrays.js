b5383c333f8ffd55ff9cb8a62df2e8b72c5c99ac
heewonhan <hhan@mail.sfsu.edu>
2019-02-06 21:16:55 -0800

    Initial commit

--- node/arrays.js
+++ node/arrays.js
@@ -1,7 +1,7 @@
 // Arrays in js
 
 // arrays can be delared explicitly
-const array = [9, 3, 4];
+
 
 // or with new keyword
 const anotherArray = new Array(5);
@@ -15,15 +15,28 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
+const array = [9, 3, 4];
+//arrayTest[5] = 9;
+console.log(arrayTest);
+arrayTest.forEach((n) => {
+    console.log(n);
 
+});
 // use map
+const newArray = aarayTest.map(i => i + 1);
+console.log(newArray);
 
 // use pop
-
+newArray.pop();
+console.log(newArray);
 // use push
-
+newArray.push(6);
+console.log(newArray);
 // use shift
-
+newArray.shift();
+console.log(newArray);
 // use unshift
+newArray.unshift(9);
+console.log(newArray);
 
 // use filter
--- node/functions.js
+++ node/functions.js
@@ -1,19 +1,31 @@
 // Declaring with the function keyword
+
+hi();
+
 function hi() {
   console.log('Say Hello');
 }
 
-hi();
+
 
 // arrow functions
 const arrow = (test) => {
   console.log(test);
 }
 
-arrow('hi')
+arrow('hi');
 
 // for practice
 
 // make an arrow function with no block body
 
+const myArrow = (y) => y + 1;
+
+console.log(myArrow(8));
 // make a function that returns a function
+const functionFactory = () => () => console.log('This is wierd');
+const a = functionFactory();
+a();
+const anotherFunction = functionFactory;
+functionFactory = 9;
+anotherFunction()();
